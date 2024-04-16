67dbc38d50a1eeb692bf2fe4c5d828a78b998b7f
Tobechi Nwachukwu <tobechid6@gmail.com>
2024-04-15 22:37:19 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { db } from "@/db";
+import Image from "next/image";
+
+export default async function Home() {
+  const items = await db.query.testing.findMany();
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {items.map((item) => {
+        return <div key={item.id}>{item.name}</div>;
+      })}
+    </main>
+  );
+}
